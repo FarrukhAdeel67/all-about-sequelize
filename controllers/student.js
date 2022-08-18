458a695329efc120b7e908040304418da80d47ad
Farrukh Adeel <81711287+FarrukhAdeel67@users.noreply.github.com>
2022-08-18 13:18:12 -0700

    code formatted

--- controllers/student.js
+++ controllers/student.js
@@ -1,13 +1,13 @@
 const Sequelize = require("sequelize");
-const Students = require("../models/student")
-const sequelize = require('../models/index')
+const Students = require("../models/student");
+const sequelize = require("../models/index");
 const moment = require("moment");
 // async function createtable(req,res){
 //   sequelize
 //   .sync({force:true})
 //   .then((result)=>{
 //     return Students.create({
-//       name:'farrukh adeel',email:'m.farrukhadeel@gmail.com',writer:'frk', 
+//       name:'farrukh adeel',email:'m.farrukhadeel@gmail.com',writer:'frk',
 //     }).then((customer)=>{
 //       console.log('first entry added to database',customer);
 //     }).catch((err)=>{
@@ -18,27 +18,26 @@ const moment = require("moment");
 
 async function post(req, res) {
   try {
-    const { name, email,writer } = req.body;
+    const { name, email, writer } = req.body;
     if (!name || !email) {
       return res.status(400).send("Required fields can't be empty");
     }
 
-   const emailfound =  await Students.findOne({
+    const emailfound = await Students.findOne({
       where: {
-        email:email,
-      }
-    })
-    if(emailfound){
-      return res.status(409).send("email already exists...")
+        email: email,
+      },
+    });
+    if (emailfound) {
+      return res.status(409).send("email already exists...");
     }
-  const student = await Students.create({
-    name: name,
-    email: email,
-    writer: writer,
-
-   });
-    res.status(200).send({student});
-    console.log({student})
+    const student = await Students.create({
+      name: name,
+      email: email,
+      writer: writer,
+    });
+    res.status(200).send({ student });
+    console.log({ student });
   } catch (err) {
     console.log(err);
     res.status(500).send("Something went wrong....");
@@ -47,8 +46,8 @@ async function post(req, res) {
 
 async function getAll(req, res) {
   try {
-     const students = await Students.findAll()
-    res.status(200).send({ students});
+    const students = await Students.findAll();
+    res.status(200).send({ students });
   } catch (err) {
     console.log(err);
     res.status(500).send("Something went wrong!");
@@ -59,9 +58,9 @@ async function getSingle(req, res) {
   try {
     const { studentId } = req.params;
     const student = await Students.findOne({
-      where:{id: studentId},
-    })
-    res.status(200).send({student});
+      where: { id: studentId },
+    });
+    res.status(200).send({ student });
   } catch (err) {
     console.log(err);
     res.status(500).send("Something went wrong!");
@@ -69,27 +68,28 @@ async function getSingle(req, res) {
 }
 
 async function updated(req, res) {
-  console.log('farrukh')
   try {
     const { name } = req.body;
     const { studentId } = req.params;
     //console.log(name);
     if (!name) {
-      return res.status(400).send("Name is required");
+      return res.status(400).send("Name is required...");
     }
     const studentFound = await Students.findOne({
       where: {
-        id: studentId
+        id: studentId,
       },
     });
     console.log(studentFound);
-    const student = await Students.update({
-      name: name,
-      
-    },{
-      where: {id:studentId },
-      individualHooks:true
-    })
+    const student = await Students.update(
+      {
+        name: name,
+      },
+      {
+        where: { id: studentId },
+        individualHooks: true,
+      }
+    );
     res.status(200).send({ student });
   } catch (err) {
     console.log(err);
@@ -101,7 +101,7 @@ async function deleteStudent(req, res) {
   try {
     const { studentId } = req.params;
     await Students.destroy({
-      where:{id:studentId},
+      where: { id: studentId },
     });
     res.status(200).send("Student deleted successfully");
   } catch (err) {
@@ -118,25 +118,29 @@ async function login(req, res) {
     }
 
     let student = await Students.findOne({
-      where:{ email }
+      where: { email },
     });
-    if(!student) {
+    if (!student) {
       return res.status(404).send("Email doesn't exist");
     }
 
     const result = await bcrypt.compare(password, student.password);
-    if(!result) {
+    if (!result) {
       return res.status(401).send("Password is incorrecct");
     }
 
     student = student.toJSON();
     delete student.password;
-    const token = jwt.sign({
-      student: student
-    }, "first-token")
+    const token = jwt.sign(
+      {
+        student: student,
+      },
+      "first-token"
+    );
 
     res.status(200).send({
-      token, student
+      token,
+      student,
     });
   } catch (err) {
     console.log(err);
@@ -145,11 +149,11 @@ async function login(req, res) {
 }
 //hooks: to run a function at a specific time
 module.exports = {
- // createtable,
+  // createtable,
   post,
   getAll,
   getSingle,
   updated,
   deleteStudent,
-  login
+  login,
 };
